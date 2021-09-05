d4bd9071a1936d9b4202d87a561a8beeee0d1857
Corey Frederickson <coreyfrederickson@Coreys-MacBook-Pro.local>
2021-09-05 13:15:38 -0500

    started api setup started App.js

--- interview-ex/src/App.js
+++ interview-ex/src/App.js
@@ -1,9 +1,21 @@
 import React, {useState} from "react";
-function App() {
-  return (
-    <div>
+import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom"
 
-    </div>
+function App() {
+  
+  const [searchHistory, setSearchHistory] = useState()
+  
+  
+  return (
+    <Router>
+      <nav>
+        <h1>Search</h1>
+        <div>
+          <Link to='/'>Back</Link>
+          <Link to='/history'>Search History</Link>
+        </div>
+      </nav>
+    </Router>
   );
 }
 
---
+++ interview-ex/src/api/index.js
@@ -0,0 +1,6 @@
+
+export const getSearchResults = async ()=>{
+    try{
+        const response = await fetch ()
+    }
+}
