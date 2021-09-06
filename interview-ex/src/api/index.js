403be2a8e2651d71e3ebd7487d0bf8cfb5342db6
Corey Frederickson <coreyfrederickson@Coreys-MBP.lan>
2021-09-06 10:35:53 -0500

    Set up api

--- interview-ex/src/api/index.js
+++ interview-ex/src/api/index.js
@@ -1,6 +1,22 @@
+const uRL = "http://hn.algolia.com/api/v1/search?query="
+const mostRecentURL = "http://hn.algolia.com/api/v1/search_by_date?query="
 
-export const getSearchResults = async ()=>{
+export const getSearchResults = async (searchArr)=>{
     try{
-        const response = await fetch ()
+        const response = await fetch (uRL+searchArr);
+        const data = response.json();
+        return data
+    }catch(error){
+        console.error(error)
+    }
+}
+
+export const getSearchResultsByDate = async (searchArr)=>{
+    try{
+        const response = await fetch (mostRecentURL+searchArr);
+        const data = response.json();
+        return data
+    }catch(error){
+        console.error(error)
     }
 }
